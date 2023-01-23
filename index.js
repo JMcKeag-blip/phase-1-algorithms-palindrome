db3190cb449fd516e8acd1a74af5cb2063417cd7
James McKeag <mckeag.james@yahoo.com>
2023-01-23 11:41:31 -0700

    commit tree 69169bbf423044c99aa1c3199eab92cdee39d082

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // match each letter to the correponding letter
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -20,6 +18,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
